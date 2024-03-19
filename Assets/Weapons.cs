6cabb5654cf195a1b5a4b9df32844eef67711359
ZedArray <hafidzuddin.ra@gmail.com>
2024-03-19 15:28:25 +0700

    added some stuff

---
+++ Assets/Weapons.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapons : MonoBehaviour
+{
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] Transform shotPoint;
+
+    public float bulletSpeed = 40f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject bullet = Instantiate(bulletPrefab, shotPoint.position, shotPoint.rotation);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            rb.AddForce(shotPoint.right * bulletSpeed, ForceMode2D.Impulse);
+        }
+    }
+}
